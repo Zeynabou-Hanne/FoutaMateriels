f69190953944a55ec884f91c7e91e3084945b6ba
Zeynabou <hanne.zeynabou@ugb.edu.sn>
2024-05-20 03:39:18 +0000

    nouveau

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'FoutaMateriels';
+  constructor(){}
+  ngOnInit(){
+
+  }
+  url:string="../assets/img/400141_1.jpg"
+  changeImage(event:any){
+    this.url=event.target.src;
+  }
 }
